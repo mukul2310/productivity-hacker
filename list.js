cbe289974dd71c866f620d1f4dfa2626fd79a98b
Mukul Agrawal <mukul2310@github.com>
2021-05-28 13:33:20 +0530

    Fix #1

--- list.js
+++ list.js
@@ -1,5 +1,3 @@
-console.log("connected");
-
 var list=
 [
     {
@@ -15,18 +13,19 @@ var list=
         desc:"desc"
     }
 ]
-printing();
-function printing(){
-    var titleList = $("li");
-    for(var i=0 ; i<titleList.length ; i++){
-        console.log(titleList[i]);
+for(i in list)
+    {
+        $("#itemlist").append(`<li class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+          <div class="fw-bold">${list[i].title}</div>
+          ${list[i].desc}
+        </div>
+        <span class="badge bg-primary rounded-pill">14</span>
+      </li>`)
     }
-    console.log("Terminated");
-}
-
-function add()
-{
-    var inputTitle = $("#todoTitle").val();
+  function add()
+  {
+  var inputTitle = $("#todoTitle").val();
     var inputDesc = $("#todoDesc").val();
     console.log(inputTitle);
     console.log(inputDesc);
@@ -40,31 +39,14 @@ function add()
         console.log(list);
 
     }
-    else{
-        return;
-    }
     for(i in list)
     {
         $("#itemlist").append(`<li class="list-group-item d-flex justify-content-between align-items-start">
         <div class="ms-2 me-auto">
-          <div class="fw-bold">${list[i].title})</div>
+          <div class="fw-bold">${list[i].title}</div>
           ${list[i].desc}
         </div>
         <span class="badge bg-primary rounded-pill">14</span>
       </li>`)
     }
-        
-
-    printing();
-};
-
-// function addTodo(){
-//     // var li = $("li");
-    
-    
-//     console.log(inputTitle);
-//     console.log(inputDesc);
-//     // var newTodo = $(inputValue);
-//     // li.appendChild(newTodo);
-    
-// }
+  }
