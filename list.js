27e1ae54a292dd5851a077e60753babaa68a8e28
unknown <singhal.tushar2000@gmail.com>
2021-06-10 18:15:05 +0530

    Todo completed

--- list.js
+++ list.js
@@ -1,12 +1,22 @@
 var list = [];
 
+function remainingTasks(){
+    let cnt=0;
+    for(l in list){
+        if(!list[l].done){
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
 function showList()
 {
     $("#itemlist").empty();
     for(i in list)
     {
         $("#itemlist").append(`<li id="${list[i].id}" class="list-group-item d-flex justify-content-between align-items-start">
-        <input class="form-check-input align-item-center justify-content-center"  type="checkbox" onclick="doneTodo(t${list[i].id})"></input>
+        <input class="form-check-input align-item-center justify-content-center"  type="checkbox" onclick="doneTodo(${list[i].id},t${list[i].id})"></input>
         <div id="t${list[i].id}" class="ms-2 me-auto">
             <div class="fw-bold">${list[i].title}</div>
             ${list[i].desc}
@@ -22,13 +32,14 @@ $("#todo").on("submit",(e)=>{
     const obj = {
         title : inputTitle,
         desc: inputDesc,
-        id: list.length +1
+        id: list.length +1,
+        done: false
     }
     if(inputTitle!=""){
         list.push(obj);
         showList();
     }
-    $('#remTasks').html(list.length);
+    $('#remTasks').html(remainingTasks());
 })
 
 function removeTodo(id){
@@ -37,11 +48,20 @@ function removeTodo(id){
             list.splice(l,1);
         }
     }
-    $('#remTasks').html(list.length);
-    showList();
+    $(`#${id}`).remove();
+    $('#remTasks').html(remainingTasks());
 }
 
-function doneTodo(id){
-    console.log($(id));
+function doneTodo(id1,id){
     $(id).toggleClass("completed");
+    for(l in list){
+        // console.log('t'+list[l].id+"  "+id+" "+typeof list[l].id+" "+typeof id);
+        if(list[l].id == id1){
+            if(list[l].done)
+            list[l].done = false;
+            else
+            list[l].done=true;
+        }
+    }
+    $('#remTasks').html(remainingTasks());
 }
