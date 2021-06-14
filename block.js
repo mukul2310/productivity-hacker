477e5640a9e01186a793ae4a891789dc80cb01bb
unknown <singhal.tushar2000@gmail.com>
2021-06-14 12:50:13 +0530

    blocklist update

--- block.js
+++ block.js
@@ -1,4 +1,18 @@
-var list = [];
+let blocklist = [];
+
+function showUrl()
+{
+    $("#blocklist").empty();
+    for(i in blocklist)
+    {
+        $("#blocklist").append(`<li id="${blocklist[i].id}" class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+            <div class="fw-bold">${blocklist[i].url}</div>
+        </div>
+        <a onclick="removeUrl(${blocklist[i].id})" href="javascript:void(0)"><i class="done material-icons">delete</i></a>
+        </li>`)
+    }
+}
 
 $("#block").on("submit",(e)=>
 {
@@ -6,16 +20,19 @@ $("#block").on("submit",(e)=>
     const url = $("#blocked_url").val();
     if(url!="")
     {
-        list.push(url);
-        $("#blocklist").append(`<li class="list-group-item d-flex justify-content-between align-items-start">
-        <div class="ms-2 me-auto">
-            <div class="fw-bold">${url}</div>
-        </div>
-        <a class="badge done material-icons" onclick="removeTodo()" href="javascript:void(0)">clear</a>
-        </li>`)
+        blocklist.push({
+            id : blocklist.length + 1,
+            url : url
+        });
+        showUrl();
     }
 })
 
-function removeTodo(){
-
+function removeUrl(id){
+    for(l in blocklist){
+        if(blocklist[l].id === id){
+            blocklist.splice(l,1);
+        }
+    }
+    $(`#${id}`).remove();
 }
